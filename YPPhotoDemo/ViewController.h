2528fe56a92aa8d242221c980ce0db6996a316ad
774655048@qq.com <774655048@qq.com>
2016-07-13 10:49:45 +0800

    Initial Commit

---
+++ YPPhotoDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YPPhotoDemo
+//
+//  Created by YueWen on 16/7/13.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YPPhotoDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YPPhotoDemo
+//
+//  Created by YueWen on 16/7/13.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
